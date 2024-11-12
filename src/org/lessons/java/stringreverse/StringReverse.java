91e890207261862ad41565ccd73988d6cf284c83
Alessandro Ruggieri <alessandro.ruggieri95.ar@gmail.com>
2024-11-12 03:48:59 +0100

    Update StringReverse.java
    Added full functionality

--- src/org/lessons/java/stringreverse/StringReverse.java
+++ src/org/lessons/java/stringreverse/StringReverse.java
@@ -1,10 +1,24 @@
 package org.lessons.java.stringreverse;
 
+import java.util.Scanner;
+
 public class StringReverse {
 
 	public static void main(String[] args) {
 		
+		String stringaInvertita = "";
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Inserisci una stringa");
+		String stringa = scan.nextLine();
+		int lunghezzaStringa = stringa.length();
 		
+		for (int i = lunghezzaStringa - 1; i >= 0; i--) {
+			
+			stringaInvertita = stringaInvertita + stringa.charAt(i);
+			
+		}
+		
+		System.out.println(stringaInvertita);
 		
 	}
 
